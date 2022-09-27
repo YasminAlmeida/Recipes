1c87f52d915d0f4bc5226ba0ed818b1f138650f7
yasmin <yasmin.almeidavd@gmail.com>
2022-09-26 22:04:48 -0400

    style(Last settings for responsiveness):

--- src/View/Footer/styled.js
+++ src/View/Footer/styled.js
@@ -1,80 +1,82 @@
 import styled from 'styled-components';
 
 export const ContainFooter = styled.section`
-    height: 40vh;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: #373737;
-    background-color: #F2F4F1;
+  height: 40vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #373737;
+  background-color: #f2f4f1;
 
-    @media(max-width: 750px){
-        height: 90vh;
-        background-color:#fbfbfb;
-        flex-direction: column-reverse;
-        justify-content: center;
-    }
-`
+  @media (max-width: 750px) {
+    height: 90vh;
+    background-color: #fbfbfb;
+    flex-direction: column-reverse;
+    justify-content: center;
+  }
+`;
 
 export const BoxIcons = styled.div`
-    width: 12%;
-    margin-left:4%;
-    display: flex;
-    justify-content: space-between;
+  width: 12%;
+  margin-left: 4%;
+  display: flex;
+  justify-content: space-between;
 
-    @media(max-width: 750px){
-        width: 70%;
-        margin-left: 0;
-        height: 30vh;
-    }
-`
+  @media (max-width: 750px) {
+    width: 70%;
+    margin-left: 0;
+    height: 30vh;
+  }
+`;
 
 export const Icon = styled.img`
-    margin: 5% 10%;
-    height: 5vh;
+  margin: 20px;
+  height: 50px;
 
-    @media(max-width: 750px){
-        height: 14vh;
-    }
-`
+  @media (max-width: 750px) {
+    height: 40px;
+  }
+`;
 
 export const BoxList = styled.div`
-    width: 30%;
-    margin-right: 5%;
-    display: flex;
-    justify-content: space-around;
+  width: 30%;
+  margin-right: 5%;
+  display: flex;
+  justify-content: space-around;
 
-    @media(max-width: 750px){
-        width: 40%;
-        height: 60vh;
-    }
-`
+  @media (max-width: 750px) {
+    width: 40%;
+    height: 60vh;
+  }
+`;
 
 export const List = styled.ul`
-    display:flex;
-    justify-content: space-between;
-    width: 100%;
-    text-transform: uppercase;
-    list-style: none;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  text-transform: uppercase;
+  list-style: none;
 
-    li{
-    border: solid rgba(255,255,255,0);
-    width: 28%;
+  li {
+    border: solid rgba(255, 255, 255, 0);
+    width: max-content;
     display: flex;
     justify-content: center;
-    }
-    li:hover{
-        border: solid ;
-    }
+    cursor: pointer;
+    padding: 5px;
+  }
+  li:hover {
+    border: solid 1px #000;
+  }
 
-    @media(max-width: 750px){
-        justify-content: space-evenly;
-        align-items: center;
-        flex-direction: column;
-        font-size: 1.8rem;
-        letter-spacing: 2.52px;
-        li{
-            width: 100%;
-        }
+  @media (max-width: 750px) {
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: column;
+    font-size: 1.8rem;
+    letter-spacing: 2.52px;
+    li {
+      width: 100%;
     }
-`
+  }
+`;
--- src/View/Header/styled.js
+++ src/View/Header/styled.js
@@ -2,136 +2,118 @@ import styled from 'styled-components';
 import fundo from '../../Img/Fundo.png';
 
 export const ContainerHeader = styled.section`
-    background-image: url(${fundo});
-    background-size: 100%;
-    background-repeat: no-repeat;
-    margin:1%;
-    height: 120vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    color: #373737;
-    background-position:center;
+  background-image: url(${fundo});
+  background-size: 100%;
+  background-repeat: no-repeat;
+  margin: 1%;
+  height: 120vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #373737;
+  background-position: center;
 
-    @media(max-width: 750px){
-        background-position: right;
-        background-size: cover;
-        height: 180vh;
-    }
-`
-
-// export const But = styled.button`
-//     width: 0.5vw;
-//     display:none;
-
-//     :hover{
-//         cursor:pointer;
-//     }
-
-//     @media(max-width: 750px){
-//         width: 10vw;
-//         height: 3vh;
-//         background: none;
-//         border: solid #373737 5px;
-//         display:initial;
-//         justify-content: flex-end;
-//     }
-// `
+  @media (max-width: 750px) {
+    background-position: right;
+    background-size: cover;
+    height: 180vh;
+  }
+`;
 
 export const BoxNav = styled.div`
-    width:90%;
-    height: 10vh;
-    margin-top: 5vh;
-    display:flex;
-    align-items: center;
-    justify-content: space-between;
+  width: 90%;
+  height: 10vh;
+  margin-top: 5vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    @media(max-width: 750px){
-        justify-content: flex-start;
-        flex-direction: column;
-        align-items: flex-end;
-        height: 30vh;
-    }
-`
+  @media (max-width: 750px) {
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-end;
+    height: 30vh;
+  }
+`;
 
 export const Logo = styled.img`
-    width:4%;
-    @media(max-width: 750px){
-        display: none;
-    }
-`
+  width: 4%;
+  @media (max-width: 750px) {
+    display: none;
+  }
+`;
 
 export const Menu = styled.div`
-    display: none;
+  display: none;
 
-    @media(max-width:750px){
-        display: flex;
-        justify-content: space-between;
-        flex-direction: column;
-        width:7vw;
-        height: 3vh;
-        cursor: poiter;
-    
+  @media (max-width: 750px) {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 7vw;
+    height: 3vh;
+    cursor: poiter;
 
-    div{
-        background-color: #373737;
-        height: 30%;
-        width: 100%;
+    div {
+      background-color: #373737;
+      height: 30%;
+      width: 100%;
     }
-}
-`
+  }
+`;
 
 export const List = styled.ul`
-    width: 45%;
-    height: 90%;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    list-style: none;
+  width: 45%;
+  height: 90%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  list-style: none;
+  cursor: pointer;
 
-    @media(max-width: 750px){
-        display: ${(props) => props.show === true ? 'flex' : 'none'};
-        width: 15%;
-        flex-direction: column;
-    }
-`
+  @media (max-width: 750px) {
+    display: ${(props) => (props.show === true ? 'flex' : 'none')};
+    width: 15%;
+    flex-direction: column;
+  }
+`;
 
 export const Li = styled.li`
-    width: 20%;
-    height: 10%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 4% 6%;
-    border:solid rgba(255,255,255,0);
-    border-spacing: 15.75px;
-    font-size: 1.1rem;
-    font-weight: 450;
-    text-transform: uppercase;
-    font: normal normal normal 24px/32px Hurme Geometric Sans 1;
-    letter-spacing: 2.52px;
-    opacity: 1;
+  width: 20%;
+  height: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4% 6%;
+  border: solid rgba(255, 255, 255, 0);
+  border-spacing: 15.75px;
+  font-size: 1.1rem;
+  font-weight: 450;
+  text-transform: uppercase;
+  font: normal normal normal 24px/32px Hurme Geometric Sans 1;
+  letter-spacing: 2.52px;
+  opacity: 1;
 
-    :hover{
-        border:solid 3px ;
-    }
+  :hover {
+    border: solid 2px;
+  }
 
-    @media(max-width: 750px){
-        width: 140%;
-    }
-`
+  @media (max-width: 750px) {
+    width: 140%;
+  }
+`;
 
 export const Title = styled.h1`
-    font-size:10vw;
-    font-weight: 500;
-    letter-spacing: 15.75px;
-    font: normal normal 600 150px/191px Hurme Geometric Sans 1;
-    text-transform: uppercase;
-    margin-top: 22%;
-    opacity: 1;
+  font-size: 10vw;
+  font-weight: 500;
+  letter-spacing: 15.75px;
+  font: normal normal 600 150px/191px Hurme Geometric Sans 1;
+  text-transform: uppercase;
+  margin-top: 22%;
+  opacity: 1;
 
-    @media (max-width: 750px){
-        margin-top: 45%;
-        letter-spacing: 10px;
-    }
-`
+  @media (max-width: 750px) {
+    margin-top: 45%;
+    letter-spacing: 10px;
+  }
+`;
